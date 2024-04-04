5d39cb647b06683209c5b00743642acebf2f2243
Peterson de Almeida Oenning <oenningpeterson9@gmail.com>
2024-04-03 22:05:30 -0400

    inserção de arquivo de migração / models / controller

--- start/routes.ts
+++ start/routes.ts
@@ -7,7 +7,17 @@
 |
 */
 
+import VeiculosController from '#controllers/veiculos_controller'
 import router from '@adonisjs/core/services/router'
+import { create } from 'domain'
 
 router.on('/').render('pages/home')
+
+
+
 router.on('/autor').render('pages/autor')
+
+router.resource('veiculos', VeiculosController)
+
+router.on('/create').render('pages/veiculos/create')
+router.on('/index').render('pages/veiculos/index')
